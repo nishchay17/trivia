2b85d771b34f58127955051cff79e5bee6728f92
nishchay17 <nishchay13971@gmail.com>
2020-05-30 20:47:31 +0530

    Updates

---
+++ precache-manifest.e98b230dae4fe121646eabd3bcc22648.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "deb8b4f7ce85d2845cf9f6fab0e035c0",
+    "url": "/trivia/index.html"
+  },
+  {
+    "revision": "e7eae3f7837a8a2b6c47",
+    "url": "/trivia/static/css/main.1f2516df.chunk.css"
+  },
+  {
+    "revision": "b1e9317964d7380ef96e",
+    "url": "/trivia/static/js/2.55f9c677.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/trivia/static/js/2.55f9c677.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e7eae3f7837a8a2b6c47",
+    "url": "/trivia/static/js/main.0d1719b6.chunk.js"
+  },
+  {
+    "revision": "f40d299287008199bdc4",
+    "url": "/trivia/static/js/runtime-main.c48cb86f.js"
+  },
+  {
+    "revision": "c297d476c1398ae4af4aef6d96088e23",
+    "url": "/trivia/static/media/Drake.c297d476.png"
+  },
+  {
+    "revision": "d9f4e81cf901d2cba65605f07bd57846",
+    "url": "/trivia/static/media/Pooh.d9f4e81c.jpg"
+  },
+  {
+    "revision": "db89d9604d93ae0ceb93e3a9cd3c6a62",
+    "url": "/trivia/static/media/carry.db89d960.png"
+  },
+  {
+    "revision": "57d2e61064ebb1a9d6027c387bc2ab5e",
+    "url": "/trivia/static/media/error.57d2e610.gif"
+  },
+  {
+    "revision": "72c3b954b65b5ef6b8e5e7b5aed32787",
+    "url": "/trivia/static/media/irfan.72c3b954.jpg"
+  }
+]);
